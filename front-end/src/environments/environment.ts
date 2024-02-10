70143540a720e8905a56044f0d58a9e01a2ff396
BeleteEndalew <148016823+BeleteEndalew@users.noreply.github.com>
2024-02-10 12:29:13 -0500

    Update environment.ts

--- front-end/src/environments/environment.ts
+++ front-end/src/environments/environment.ts
@@ -5,8 +5,8 @@
 
 export const environment = {
   production: false,
-  resourceApi: "http://18.116.49.76/resources-api",
-  inventoryApi: "http://18.116.49.76/inventory-api",
-  clientApi: "http://18.116.49.76/clients-api",
-  rentingApi: "http://18.116.49.76/renting-api"
+  resourceApi: "http://3.137.179.43/resources-api",
+  inventoryApi: "http://3.137.179.43/inventory-api",
+  clientApi: "http://3.137.179.43/clients-api",
+  rentingApi: "http://3.137.179.43/renting-api"
 };
